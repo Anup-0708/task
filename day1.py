f8a084a170fabe3c5a418911f691af8597bbf542
Anupam Mehetre <137478160+Anup-0708@users.noreply.github.com>
2023-06-23 12:15:30 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,13 @@
+name="Anupam Mehetre"
+std="TYCM"
+div="B"
+english=maths=science=100
+total=english+maths+science
+per=total/3
+print("RESULT")
+print("Name: {}  Std: {}  Div: {}".format(name,std,div))
+print("____________________________________________________")
+print("Enlish : {}\nMaths : {}\nScience : {}".format(english,maths,science))
+print("Total: {}\t\tPercentage: {}".format(total,per))
+print("____________________________________________________")
+print("____________________________________________________")
