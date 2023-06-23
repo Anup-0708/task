504189e6e06d1b7401fa46a6339b6758fb08855d
Anupam Mehetre <137478160+Anup-0708@users.noreply.github.com>
2023-06-23 13:09:27 +0530

    Add files via upload

---
+++ Arthamatic Operation.py
@@ -0,0 +1,12 @@
+a=10
+b=12
+#Addition
+print("Adition Of A and B is: ",a+b)
+#Substraction
+print("Substraction Of A and B is: ",a-b)
+#Multiplication
+print("Multiplication Of A and B is: ",a*b)
+#Division
+print("Division Of A and B is: ",a/b)
+
+
