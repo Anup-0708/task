70ff70f5823aa1273b6e9394800372d8a260f811
Anupam Mehetre <137478160+Anup-0708@users.noreply.github.com>
2023-06-26 21:54:08 +0530

    Add files via upload

---
+++ precendance of operator.py
@@ -0,0 +1,7 @@
+a=12
+b=12
+c=12
+d=10
+print(not a==b or a==c and a==c)
+#not operator perform first and "and" operator has high precendance and then "or" operator
+# here first execute not operator it is false then and operator it is true and then or operator one condition is true so output is true
